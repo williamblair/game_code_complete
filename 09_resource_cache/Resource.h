ec7aa4585668253cfd29428c50c18641c50ca8e0
wblair <wblair@androcs.com>
2021-05-13 16:53:27 -0400

    saving 09 cache status

---
+++ 09_resource_cache/Resource.h
@@ -0,0 +1,18 @@
+#ifndef RESOURCE_H_INCLUDED
+#define RESOURCE_H_INCLUDED
+
+#include <string>
+
+class Resource
+{
+public:
+
+    Resource( const std::string& name );
+
+    std::string m_name;
+
+private:
+};
+
+#endif // RESOURCE_H_INCLUDED
+
---
+++ 09_resource_cache/main.cpp
@@ -0,0 +1,9 @@
+#include <Resource.h>
+#include <IResourceFile.h>
+
+int main()
+{
+
+    return 0;
+}
+
