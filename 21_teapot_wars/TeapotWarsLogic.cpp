aaa437741fcac8e04b260b1a8aa9b0d6daa23b27
williamblair <bblair12876@aol.com>
2023-12-26 23:10:20 -0500

    start teapot logic impl, physics component

--- 21_teapot_wars/TeapotWarsLogic.cpp
+++ 21_teapot_wars/TeapotWarsLogic.cpp
@@ -1,27 +1,50 @@
 #include <GCC4/Actor.h>
 #include <GCC4/EventManager.h>
 #include <GCC4/NetworkGameView.h>
+#include <GCC4/MakeStrongPtr.h>
+#include <GCC4/TransformComponent.h>
 #include "AITeapotView.h"
 #include "TeapotWarsHumanView.h"
+#include "TeapotEvents.h"
 #include "TeapotWarsLogic.h"
 
 TeapotWarsLogic::TeapotWarsLogic()
 {
     //TODO
+    //m_pPhysics.reset(CreateGamePhysics());
+    RegisterAllDelegates();
 }
 TeapotWarsLogic::~TeapotWarsLogic()
 {
-    //TODO
+    RemoveAllDelegates();
+    DestroyAllNetworkEventForwarders();
 }
 
 // Update
 void TeapotWarsLogic::VSetProxy()
 {
-    //TODO
+    // FUTURE WORK: this can go in base game logic
+    BaseGameLogic::VSetProxy();
 }
 void TeapotWarsLogic::VMoveActor(const ActorId id, Mat4x4 const& mat)
 {
-    //TODO
+    BaseGameLogic::VMoveActor(id, mat);
+
+    // FUTURE WORK: this would make a great basis for a trigger actor that ran a
+    // LUA script when other actors entered or left it
+    
+    StrongActorPtr pActor = MakeStrongPtr(VGetActor(id));
+    if (pActor) {
+        std::shared_ptr<TransformComponent> pTransformComponent = MakeStrongPtr(
+            pActor->GetComponent<TransformComponent>(TransformComponent::g_Name)
+        );
+        if (pTransformComponent && pTransformComponent->GetPosition().y < -25) {
+            std::shared_ptr<EvtDataDestroyActor> pDestroyActorEvent(
+                new EvtDataDestroyActor(id)
+            );
+            IEventManager::GetInstance()->VQueueEvent(pDestroyActorEvent);
+        }
+    }
 }
 
 // Overloads
@@ -129,49 +152,238 @@ void TeapotWarsLogic::VAddView(
     std::shared_ptr<IGameView> pView,
     ActorId actorId)
 {
-    //TODO
+    BaseGameLogic::VAddView(pView, actorId);
+
+    if (std::dynamic_pointer_cast<TeapotWarsHumanView>(pView)) {
+        m_HumanPlayersAttached++;
+    }
+    else if (std::dynamic_pointer_cast<AITeapotView>(pView)) {
+        m_AiPlayersAttached++;
+    }
 }
 
 
 void TeapotWarsLogic::RequestStartGameDelegate(IEventDataPtr pEventData)
 {
-    //TODO
-}
-
-void TeapotWarsLogic::GameStateDelegate(IEventDataPtr pEventData)
-{
-    //TODO
+    (void)pEventData;
+    VChangeState(BGS_WaitingForPlayers);
 }
 
 void TeapotWarsLogic::RemoteClientDelegate(IEventDataPtr pEventData)
 {
-    //TODO
+    // This event is always sent from clients to the game server
+
+    std::shared_ptr<EvtDataRemoteClient> pCastEventData =
+        std::static_pointer_cast<EvtDataRemoteClient>(pEventData);
+    const int sockID = pCastEventData->GetSocketId();
+    const int ipAddress = pCastEventData->GetIpAddress();
+
+    // go find a NetworkView that doesn't have a socket ID,
+    // and attach this client to it
+    for (auto it = m_gameViews.begin(); it != m_gameViews.end(); ++it) {
+        std::shared_ptr<IGameView> pView = *it;
+        if (pView->VGetType() == GameView_Remote) {
+            std::shared_ptr<NetworkGameView> pNetworkGameView =
+                std::static_pointer_cast<NetworkGameView,IGameView>(pView);
+            if (!pNetworkGameView->HasRemotePlayerAttached()) {
+                pNetworkGameView->AttachRemotePlayer(sockID);
+                CreateNetworkEventForwarder(sockID);
+                m_HumanPlayersAttached++;
+
+                return;
+            }
+        }
+    }
 }
 
 void TeapotWarsLogic::NetworkPlayerActorAssignmentDelegate(IEventDataPtr pEventData)
 {
-    //TODO
+    if (!m_bProxy) {
+        return;
+    }
+
+    // we're a remote client getting an actor assignment
+    // the server assigned us a playerId when we first attached (the server's socketId, actually)
+    std::shared_ptr<EvtDataNetworkPlayerActorAssignment> pCastEventData =
+        std::static_pointer_cast<EvtDataNetworkPlayerActorAssignment>(pEventData);
+    if (pCastEventData->GetActorId() == INVALID_ACTOR_ID) {
+        m_remotePlayerId = pCastEventData->GetSocketId();
+        return;
+    }
+
+    for (auto it = m_gameViews.begin(); it != m_gameViews.end(); ++it) {
+        std::shared_ptr<IGameView> pView = *it;
+        if (pView->VGetType() == GameView_Human) {
+            std::shared_ptr<TeapotWarsHumanView> pHumanView =
+                std::static_pointer_cast<TeapotWarsHumanView,IGameView>(pView);
+            if (m_remotePlayerId == pCastEventData->GetSocketId()) {
+                pHumanView->VSetControlledActor(pCastEventData->GetActorId());
+            }
+            return;
+        }
+    }
+
+    GCC_ERROR("Could not find HumanView to attach actor to!");
 }
 
-void TeapotWarsLogic::NewGameDelegate(IEventDataPtr pEventData)
+void TeapotWarsLogic::EnvironmentLoadedDelegate(IEventDataPtr pEventData)
 {
-    //TODO
+    (void)pEventData;
+    ++m_HumanGamesLoaded;
+}
+
+void TeapotWarsLogic::StartThrustDelegate(IEventDataPtr pEventData)
+{
+    std::shared_ptr<EvtDataStartThrust> pCastEventData =
+        std::static_pointer_cast<EvtDataStartThrust>(pEventData);
+    StrongActorPtr pActor = MakeStrongPtr(VGetActor(pCastEventData->GetActorId()));
+    if (pActor) {
+        std::shared_ptr<PhysicsComponent> pPhysicalComponent =
+            MakeStrongPtr(
+                pActor->GetComponent<PhysicsComponent>(
+                    PhysicsComponent::g_Name
+                )
+            );
+        if (pPhysicalComponent) {
+            pPhysicalComponent->ApplyAcceleration(pCastEventData->GetAcceleration());
+        }
+    }
+}
+
+void TeapotWarsLogic::EndThrustDelegate(IEventDataPtr pEventData)
+{
+    std::shared_ptr<EvtDataStartThrust> pCastEventData =
+        std::static_pointer_cast<EvtDataStartThrust>(pEventData);
+    StrongActorPtr pActor = MakeStrongPtr(VGetActor(pCastEventData->GetActorId()));
+    if (pActor) {
+        std::shared_ptr<PhysicsComponent> pPhysicalComponent = MakeStrongPtr(
+            pActor->GetComponent<PhysicsComponent>(PhysicsComponent::g_Name)
+        );
+        if (pPhysicalComponent) {
+            pPhysicalComponent->RemoveAcceleration();
+        }
+    }
+}
+
+void TeapotWarsLogic::StartSteerDelegate(IEventDataPtr pEventData)
+{
+    std::shared_ptr<EvtDataStartThrust> pCastEventData =
+        std::static_pointer_cast<EvtDataStartThrust>(pEventData);
+    StrongActorPtr pActor = MakeStrongPtr(VGetActor(pCastEventData->GetActorId()));
+    if (pActor) {
+        std::shared_ptr<PhysicsComponent> pPhysicalComponent = MakeStrongPtr(
+            pActor->GetComponent<PhysicsComponent>(PhysicsComponent::g_Name)
+        );
+        if (pPhysicalComponent) {
+            pPhysicalComponent->ApplyAngularAcceleration(
+                pCastEventData->GetAcceleration()
+            );
+        }
+    }
+}
+
+void TeapotWarsLogic::EndSteerDelegate(IEventDataPtr pEventData)
+{
+    std::shared_ptr<EvtDataStartThrust> pCastEventData =
+        std::static_pointer_cast<EvtDataStartThrust>(pEventData);
+    StrongActorPtr pActor = MakeStrongPtr(VGetActor(pCastEventData->GetActorId()));
+    if (pActor) {
+        std::shared_ptr<PhysicsComponent> pPhysicalComponent = MakeStrongPtr(
+            pActor->GetComponent<PhysicsComponent>(PhysicsComponent::g_Name)
+        );
+        if (pPhysicalComponent) {
+            pPhysicalComponent->RemoveAngularAcceleration();
+        }
+    }
+}
+
+void TeapotWarsLogic::TestScriptDelegate(IEventDataPtr pEventData)
+{
+    std::shared_ptr<EvtDataScriptEventTestFromLua> pCastEventData =
+        std::static_pointer_cast<EvtDataScriptEventTestFromLua>(pEventData);
+    GCC_LOG("Lua", "Event received in C++ from Lua: " + std::to_string(pCastEventData->GetNum()));
 }
 
 bool TeapotWarsLogic::VLoadGameDelegate(tinyxml2::XMLElement* pLevelData)
 {
-    //TODO
+    RegisterTeapotScriptEvents();
     return true;
 }
 
 void TeapotWarsLogic::RegisterAllDelegates()
 {
-    //TODO
+    // FUTURE WORK: Lots of these functions are ok to go into the base game logic!
+
+    m_RemoteClientDelegate.reset(
+        DECL_MBR_DELEGATE(&TeapotWarsLogic::RemoteClientDelegate)
+    );
+    m_MoveActorDelegate.reset(
+        DECL_MBR_DELEGATE(&TeapotWarsLogic::MoveActorDelegate)
+    );
+    m_RequestStartGameDelegate.reset(
+        DECL_MBR_DELEGATE(&TeapotWarsLogic::RequestStartGameDelegate)
+    );
+    m_NetworkPlayerActorAssignmentDelegate.reset(
+        DECL_MBR_DELEGATE(&TeapotWarsLogic::NetworkPlayerActorAssignmentDelegate)
+    );
+    m_EnvironmentLoadedDelegate.reset(
+        DECL_MBR_DELEGATE(&TeapotWarsLogic::EnvironmentLoadedDelegate)
+    );
+
+    IEventManager* pGlobalEventManager = IEventManager::GetInstance();
+    pGlobalEventManager->VAddListener(m_RemoteClientDelegate, EvtDataRemoteClient::sk_EventType);
+    pGlobalEventManager->VAddListener(m_MoveActorDelegate, EvtDataMoveActor::sk_EventType);
+    pGlobalEventManager->VAddListener(m_RequestStartGameDelegate, EvtDataRequestStartGame::sk_EventType);
+    pGlobalEventManager->VAddListener(m_NetworkPlayerActorAssignmentDelegate, EvtDataNetworkPlayerActorAssignment::sk_EventType);
+    pGlobalEventManager->VAddListener(m_EnvironmentLoadedDelegate, EvtDataEnvironmentLoaded::sk_EventType);
+    pGlobalEventManager->VAddListener(m_EnvironmentLoadedDelegate, EvtDataRemoteEnvironmentLoaded::sk_EventType);
+
+    // FUTURE WORK: only these belong in TeapotWars.
+    
+    m_StartThrustDelegate.reset(
+        DECL_MBR_DELEGATE(&TeapotWarsLogic::StartThrustDelegate)
+    );
+    m_EndThrustDelegate.reset(
+        DECL_MBR_DELEGATE(&TeapotWarsLogic::EndThrustDelegate)
+    );
+    m_StartSteerDelegate.reset(
+        DECL_MBR_DELEGATE(&TeapotWarsLogic::StartSteerDelegate)
+    );
+    m_EndSteerDelegate.reset(
+        DECL_MBR_DELEGATE(&TeapotWarsLogic::EndSteerDelegate)
+    );
+    m_TestScriptDelegate.reset(
+        DECL_MBR_DELEGATE(&TeapotWarsLogic::TestScriptDelegate)
+    );
+
+    pGlobalEventManager->VAddListener(m_StartThrustDelegate, EvtDataStartThrust::sk_EventType);
+    pGlobalEventManager->VAddListener(m_EndThrustDelegate, EvtDataEndThrust::sk_EventType);
+    pGlobalEventManager->VAddListener(m_StartSteerDelegate, EvtDataStartSteer::sk_EventType);
+    pGlobalEventManager->VAddListener(m_EndSteerDelegate, EvtDataEndSteer::sk_EventType);
+
+    pGlobalEventManager->VAddListener(m_TestScriptDelegate, EvtDataScriptEventTestFromLua::sk_EventType);
 }
 
 void TeapotWarsLogic::RemoveAllDelegates()
 {
+    IEventManager* pGlobalEventManager = IEventManager::GetInstance();
+    pGlobalEventManager->VRemoveListener(m_RemoteClientDelegate, EvtDataRemoteClient::sk_EventType);
+    pGlobalEventManager->VRemoveListener(m_MoveActorDelegate, EvtDataMoveActor::sk_EventType);
+    pGlobalEventManager->VRemoveListener(m_RequestStartGameDelegate, EvtDataRequestStartGame::sk_EventType);
+    pGlobalEventManager->VRemoveListener(m_NetworkPlayerActorAssignmentDelegate, EvtDataNetworkPlayerActorAssignment::sk_EventType);
+    pGlobalEventManager->VRemoveListener(m_EnvironmentLoadedDelegate, EvtDataEnvironmentLoaded::sk_EventType);
+    pGlobalEventManager->VRemoveListener(m_EnvironmentLoadedDelegate, EvtDataRemoteEnvironmentLoaded::sk_EventType);
     //TODO
+    //if (m_bProxy) {
+    //    pGlobalEventManager->VRemoveListener(m_RequestNewActorDelegate, EvtDataRequestNewActor::sk_EventType);
+    //}
+
+    pGlobalEventManager->VRemoveListener(m_StartThrustDelegate, EvtDataStartThrust::sk_EventType);
+    pGlobalEventManager->VRemoveListener(m_EndThrustDelegate, EvtDataEndThrust::sk_EventType);
+    pGlobalEventManager->VRemoveListener(m_StartSteerDelegate, EvtDataStartSteer::sk_EventType);
+    pGlobalEventManager->VRemoveListener(m_EndSteerDelegate, EvtDataEndSteer::sk_EventType);
+
+    pGlobalEventManager->VRemoveListener(m_TestScriptDelegate, EvtDataScriptEventTestFromLua::sk_EventType);
 }
 
 void TeapotWarsLogic::CreateNetworkEventForwarder(const int socketId)
